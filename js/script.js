ad53339c4a46bc956a159ecfaf7bf1fa068e15b1
Karen Abrahamyan <juvelist@list.ru>
2023-04-09 00:44:39 +0400

    page-header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('test')
+
+let count = document.getElementById('count')
+let counter = 0
+setInterval(() => {
+    if(counter === 20) {
+        clearInterval()
+    } else {
+        counter += 1
+        count.innerHTML = counter + '%'
+    }
+}, 30)

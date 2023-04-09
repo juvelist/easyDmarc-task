0c52e9b701e90f11719cf0992807c2573fd3833f
Karen Abrahamyan <juvelist@list.ru>
2023-04-09 16:52:05 +0400

    sidebar full responsive

--- js/script.js
+++ js/script.js
@@ -1,12 +1,51 @@
-console.log('test')
+const toggleSidebarOnMobile = () => {
+  const sidebarBtn = document.getElementById('sidebarBtn')
+  const sidebar = document.getElementById('sidebar')
+  sidebarBtn.addEventListener('click', (e) => {
+    sidebarBtn.classList.toggle('show')
+    sidebar.classList.toggle('show')
+  })
+}
 
-let count = document.getElementById('count')
-let counter = 0
-setInterval(() => {
-    if(counter === 20) {
-        clearInterval()
+const loadProgress = () => {
+  let count = document.getElementById('count')
+  let counter = 0
+  setInterval(() => {
+    if (counter === 20) {
+      clearInterval()
     } else {
-        counter += 1
-        count.innerHTML = counter + '%'
+      counter += 1
+      count.innerHTML = counter + '%'
     }
-}, 30)
+  }, 30)
+}
+
+const collapseListItems = () => {
+  const collapseItems = document.querySelectorAll('.sidebar__list_link.collapse')
+  if(collapseItems) {
+    collapseItems.forEach(collapseItem => {
+      collapseItem.addEventListener('click', (e) => {
+        e.preventDefault()
+        const content = e.target.nextElementSibling
+        if (e.target.classList.contains('collapsed')) {
+          e.target.classList.remove('collapsed')
+          content.style.maxHeight = null
+        } else {
+          e.target.classList.add('collapsed')
+          content.style.maxHeight = content.scrollHeight + 'px'
+        }
+      })
+    })
+  }
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  loadProgress()
+  collapseListItems()
+  toggleSidebarOnMobile()
+});
+
+
+
+
+
